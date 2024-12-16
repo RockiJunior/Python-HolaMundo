893264de24b0b0b2232fb4cbd6712f2849d50647
Gabriel Villarroel <gabriel@latechfactory.com>
2024-12-15 22:57:25 -0300

    section 3 finished

---
+++ testDecode/decode.py
@@ -0,0 +1,8 @@
+# this is a test for an exam to a job
+
+import base64
+
+coded_code = "aHR0cHM6Ly90bnM0bHBnbXppaXlwbnh4emVsNXNzNW55dTBuZnRvbC5sYW1iZGEtdXJsLnVzLWVhc3QtMS5vbi5hd3MvcmFtcC1jaGFsbGVuZ2UtaW5zdHJ1Y3Rpb25zLw=="
+url_original = base64.b64decode(coded_code)
+
+print(url_original)
